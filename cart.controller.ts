3f1c6028b743712a8ea76107aef715ab67f4b9c4
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-07-07 11:14:43 +0200

    Update cart.controller.ts

--- cart.controller.ts
+++ cart.controller.ts
@@ -2,21 +2,13 @@ import type { Request, Response, NextFunction } from "express";
 import cartSerivce from "../services/cart.serivce.js";
 import asyncHandler from "../middleware/asyncHandler.js";
 
-interface authBody {
-    userId: number,
-    productId: number,
-    quantity: number;
-}
-
 export const cartController = {
-
 
     getCart: asyncHandler(async(req: Request, res: Response) => {
         const userId = (req as any).user.id;
         const result = await cartSerivce.getCart(userId);
         return res.status(200).json(result);
     }),
-
 
     addItem: asyncHandler(async(req: Request, res: Response) => {
         const userId = (req as any).user.id;
