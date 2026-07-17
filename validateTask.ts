ccfa24d545b8976cd91d9fcbf780b4ada5b36165
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-07-17 12:57:57 +0200

    Update validateTask to check name, price, and stock

--- validateTask.ts
+++ validateTask.ts
@@ -1,12 +1,18 @@
 import type { Request, Response, NextFunction } from "express";
 
-const validateTask = (req:Request, res: Response, next: NextFunction): void => {
-    const { title, description } = req.body as {title?: unknown , description?: unknown};
+const validateTask = (req: Request, res: Response, next: NextFunction): void => {
+    const { name, price, stock } = req.body;
 
-    if (!title || typeof title != "string" || !description || typeof description != "string") {
-        const err = new Error("Ttile and description are required and must be strings") as Error & { status?: number};
-        (err as any).status = 400;
-        throw new Error;
+    if (!name || typeof name !== "string") {
+        const err = new Error("Name is required") as Error & { status?: number };
+        err.status = 400;
+        return next(err);
+    }
+
+    if (!price || isNaN(Number(price))) {
+        const err = new Error("Price is required") as Error & { status?: number };
+        err.status = 400;
+        return next(err);
     }
 
     next();
